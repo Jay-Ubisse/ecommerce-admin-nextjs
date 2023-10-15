884cd019ab7da851073653aacce53b2f7ecba344
Jay-Ubisse <joaquimubisse@gmail.com>
2023-10-15 22:41:02 +0200

    bug fix

---
+++ app/(dashboard)/[storeid]/layout.tsx
@@ -0,0 +1,35 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function DashboardLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { userId: string };
+}) {
+    const { userId } = auth();
+
+    if(!userId) {
+        redirect('/sign-in');
+    }
+
+    const store = await prismadb.store.findFirst({
+        where: {
+            userId: userId,
+            id: params.userId,
+        }
+    })
+
+    if(!store) {
+        redirect('/');
+    }
+
+    return (
+        <>
+            <div>NavBar</div>
+            {children}
+        </>
+    )
+}
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,5 @@
+export default async function SetupLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {}
